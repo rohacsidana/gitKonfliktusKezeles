c58353d6ccd74a1315f87878f2be7ba3006b66be
rohacsidana <rohacsi.dana@gmail.com>
2022-03-10 12:40:14 +0100

    3. név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,9 +3,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
-
-        String nev1 = "RD";        
+        int i = 7;
+        String nev1 = "RD";
         String nev2 = "X. Y.";
+        int nev3;
     }
 
 }
