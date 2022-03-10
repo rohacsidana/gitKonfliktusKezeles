f08db94c383687d586ecc5aa2da8287bc524a679
rohacsidana <rohacsi.dana@gmail.com>
2022-03-10 12:29:05 +0100

    1. név inicializálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,7 +3,7 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "RD";
         String nev2;
     }
 
